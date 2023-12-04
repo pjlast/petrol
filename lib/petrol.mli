379aa32b59b805c3d54f6450bce72e6500da4c63
Petri-Johan Last <petri.last@sourcegraph.com>
2023-12-04 08:49:06 +0200

    Fix missing  in vl_opt signature

--- lib/petrol.mli
+++ lib/petrol.mli
@@ -246,7 +246,7 @@ module Sqlite3 : sig
     val vl: ty:'a Type.t -> 'a -> 'a t
     (** [vl ~ty value] returns an expression that evaluates to the value [value] with type [ty]. *)
 
-    val vl_opt: ty:'a Type.t -> 'a -> 'a option t
+    val vl_opt: ty:'a Type.t -> 'a option -> 'a option t
     (** [vl_opt ~ty value] returns an expression that evaluates to the value [value option] with type [ty]. *)
 
     val i_stat : int -> int t
