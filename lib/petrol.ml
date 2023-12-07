bd472d407000032c0ab486953f4b44eaf10e7987
Petri-Johan Last <petri.last@sourcegraph.com>
2023-12-07 22:20:21 +0200

    Use only a single Schema type

--- lib/petrol.ml
+++ lib/petrol.ml
@@ -72,15 +72,6 @@ module Postgres = struct
   end
 end
 
-let result_all_unit : (unit, 'e) result list -> (unit, 'e) result =
- fun ls ->
-  let rec loop = function
-    | [] -> Ok ()
-    | Ok () :: t -> loop t
-    | Error err :: _ -> Error err
-  in
-  loop ls
-
 let exec :
     (module Caqti_lwt.CONNECTION) ->
     (unit, [< `Zero ]) Request.t ->
@@ -119,50 +110,3 @@ let collect_list :
      ((MkCaqti (inps, req), wrapp_value) : (a, _) Request.t) ->
   let data = Request.unwrap (inps, wrapp_value) in
   DB.collect_list req data
-
-module StaticSchema = struct
-  type wrapped_table =
-    | MkTable :
-        int * string * 'a Schema.table * [ `Table ] Schema.constraint_ list
-        -> wrapped_table
-
-  type t = (int, wrapped_table) Hashtbl.t
-
-  let init () : t = Hashtbl.create 10
-
-  let declare_table tables ?(constraints : _ list = []) ~name tbl =
-    List.iter Schema.ensure_table_constraint constraints;
-    let id = Hashtbl.length tables in
-    Hashtbl.add tables id (MkTable (id, name, tbl, constraints));
-    let rec to_table : 'a. string -> 'a Schema.table -> 'a Expr.expr_list =
-     fun name (type a) (table : a Schema.table) : a Expr.expr_list ->
-      match table with
-      | [] -> []
-      | (field_name, field_ty, _) :: rest ->
-          (Types.FIELD ((id, name), field_name, field_ty) : _ Expr.t)
-          :: to_table name rest
-    in
-    let table = to_table name tbl in
-    ((id, name), table)
-
-  let initialise tables (module DB : Caqti_lwt.CONNECTION) =
-    let open Lwt_result.Syntax in
-    let table_defs =
-      Hashtbl.fold
-        (fun _key (MkTable (_, name, table, constraints)) acc ->
-          List.cons (Schema.to_sql ~name table constraints) acc)
-        tables
-        ([] : 'a list)
-    in
-    let* () =
-      Lwt_list.map_s
-        (fun table_def ->
-          let req =
-            Caqti_request.Infix.(Caqti_type.unit ->. Caqti_type.unit) table_def
-          in
-          DB.exec req ())
-        table_defs
-      |> Lwt.map result_all_unit
-    in
-    Lwt_result.return ()
-end
